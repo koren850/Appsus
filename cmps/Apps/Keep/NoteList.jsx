7ff15b82e9fccf98fb59e029cf2b198516dfd93e
koren850 <korenah3@gmail.com>
2021-12-29 12:16:23 +0200

    commit

--- cmps/Apps/Keep/NoteList.jsx
+++ cmps/Apps/Keep/NoteList.jsx
@@ -1,10 +1,11 @@
+import { NotePreview } from "./NotePreview.jsx";
 export function NoteList({ notes }) {
 	if (!notes.length) return <h1>There are no books to show</h1>;
 	return (
 		<section className='note-list'>
-			{/* {notes.map((note) => (
-				// <notePreview key={note.id} note={note} />
-			))} */}
+			{notes.map((note) => (
+				<NotePreview key={note.id} note={note} />
+			))}
 		</section>
 	);
 }
