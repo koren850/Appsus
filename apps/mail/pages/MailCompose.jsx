1004a65af9288ed7a98ad196b5197ca1934217a8
koren850 <korenah3@gmail.com>
2021-12-30 17:16:33 +0200

    day 2 afternoon

--- apps/mail/pages/MailCompose.jsx
+++ apps/mail/pages/MailCompose.jsx
@@ -1,12 +1,6 @@
-<<<<<<< HEAD
 import { mailService } from "../services/mail.service.js";
 import { utilsService } from "../../../services/utils.service.js";
 import { swalService } from "../../../services/swal.service.js";
-=======
-import { mailService } from '../services/mail.service.js'
-import {utilsService} from '../../../services/utils.service.js'
-import {swalService} from '../../../services/swal.service.js'
->>>>>>> 6f5ce0dbed71ebe87aa94f681f85946c892264b9
 export class MailCompose extends React.Component {
 	createMail = (ev) => {
 		ev.preventDefault();
@@ -28,39 +22,32 @@ export class MailCompose extends React.Component {
 		swalService.userModal("success", "Mail sent");
 	};
 
-<<<<<<< HEAD
-	deleteMail = () => {
-		swalService.checkAgain(this.deleteConfirm);
+	createMail = (ev) => {
+		ev.preventDefault();
+		const values = Array.from(ev.target);
+		const mail = {
+			id: utilsService.generateId(),
+			subject: values[1].value,
+			body: values[3].value,
+			isRead: false,
+			isSent: true,
+			isStar: false,
+			isDraft: false,
+			isDeleted: false,
+			sentAt: new Date(),
+			from: mailService.getUser().mail,
+			to: values[0].value,
+		};
+		window.location.replace("/index.html#/mail");
+		mailService.addMail(mail);
+		swalService.userModal("success", "Mail sent");
 	};
-=======
-    createMail =(ev) => {
-        ev.preventDefault();
-        const values = Array.from(ev.target);
-        const mail = {
-            id: utilsService.generateId(),
-            subject: values[1].value,
-            body: values[3].value,
-            isRead: false,
-            isSent: true,
-            isStar: false,
-            isDraft: false,
-            isDeleted:false,
-            sentAt: new Date(),
-            from: mailService.getUser().mail,
-            to: values[0].value,
-        }
-        window.location.replace('/index.html#/mail');
-        mailService.addMail(mail);
-        swalService.userModal('success', 'Mail sent');
-    }
->>>>>>> 6f5ce0dbed71ebe87aa94f681f85946c892264b9
 
 	deleteConfirm = () => {
 		window.location.replace("/index.html#/mail");
 		swalService.userModal("error", "Mail Deleted");
 	};
 
-<<<<<<< HEAD
 	render() {
 		return (
 			<section className={"mail-compose container"}>
@@ -85,6 +72,7 @@ export class MailCompose extends React.Component {
 					<main></main>
 					<footer>
 						<button className={"mail-compose send"}>Send</button>
+						<button className={"mail-compose far edit"}></button>
 						<button className={"mail-compose fas trash"} type='button' onClick={this.deleteMail}></button>
 					</footer>
 				</form>
@@ -92,24 +80,3 @@ export class MailCompose extends React.Component {
 		);
 	}
 }
-=======
-    render() {
-
-        return (<section className={'mail-compose container'}>
-            <header className={'mail-compose line'}>New Message</header>
-            <form onSubmit={this.createMail}>
-            <article className={'mail-compose line'}><label htmlFor="to">To :</label><input type="email" id="to" placeholder="Example@Jmail.com."/></article>
-            <article className={'mail-compose line'}><label htmlFor="cc">Title :</label><input type="text" id="cc" placeholder="Example Example Eat's FalaFel..."/></article>
-            <article className={'mail-compose line'}><label htmlFor="bcc">Bcc :</label><input type="text" id="bcc" placeholder="Example@Jmail.com , ExampleFather@Jmail.com ,ExampleBaby@Jmail.com."/></article>
-            <article><label htmlFor="subject">Subject :</label><textarea id="subject"/></article>
-            <main></main>
-            <footer>
-            <button className={'mail-compose send'}>Send</button>
-            <button className={'mail-compose far edit'}></button>
-            <button className={'mail-compose fas trash'} type="button" onClick={this.deleteMail}></button>
-            </footer>
-            </form>
-        </section>)
-    }
- }
->>>>>>> 6f5ce0dbed71ebe87aa94f681f85946c892264b9
