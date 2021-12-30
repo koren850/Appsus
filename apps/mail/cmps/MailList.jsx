7f9ae6e428e91c6a9613194e00a36242f59c91ac
koren850 <korenah3@gmail.com>
2021-12-30 20:22:01 +0200

    bug fix

--- apps/mail/cmps/MailList.jsx
+++ apps/mail/cmps/MailList.jsx
@@ -1,20 +1,21 @@
 import { MailPreview } from "./MailPreview.jsx";
-import {mailService} from "../services/mail.service.js";
+import { mailService } from "../services/mail.service.js";
 
 export function MailList({ mails }) {
-    const { Link } = ReactRouterDOM;
+	const { Link } = ReactRouterDOM;
 
-  if (!mails.length) return <h1>There are no books to show</h1>;
-  return (
-    <section className="mail-list">
-        <Link to={'/mail/compose'}><button className={'mail-list btn'}><span className={'mail-list compose-span'}>Compose</span><span className={'mail-list fas fa-plus'}><span className={'mail-list gradient'}></span></span></button></Link>
-      {mails.map(
-        (mail) =>
-          !mail.isDeleted | mailService.checkDeletedFilter()  && 
-           (
-              <MailPreview key={mail.id} mail={mail} />
-          )
-      )}
-    </section>
-  );
+	if (!mails.length) return <h1>There are no books to show</h1>;
+	return (
+		<section className='mail-list'>
+			<Link to={"/mail/compose"}>
+				<button className={"mail-list btn"}>
+					<span className={"mail-list compose-span"}>Compose</span>
+					<span className={"mail-list fas fa-plus"}>
+						<span className={"mail-list gradient"}></span>
+					</span>
+				</button>
+			</Link>
+			{mails.map((mail) => (!mail.isDeleted || mailService.checkDeletedFilter()) && <MailPreview key={mail.id} mail={mail} />)}
+		</section>
+	);
 }
