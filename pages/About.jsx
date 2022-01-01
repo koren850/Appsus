dd633a039244ded466d0cd26701feed606d831bc
koren850 <korenah3@gmail.com>
2022-01-01 21:24:09 +0200

    iiimg

--- pages/About.jsx
+++ pages/About.jsx
@@ -23,21 +23,5 @@ function Vision() {
 }
 
 export function About() {
-	return (
-		<section className='about'>
-			<h1>We're all about books...</h1>
-			<p>
-				Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odit earum tenetur, accusantium adipisci, provident
-				reprehenderit eaque similique quia nemo commodi et laboriosam ab est sunt dolor velit repellat illum placeat.
-			</p>
-			<NavLink activeClassName='my-active' to='/about/team'>
-				My Team
-			</NavLink>
-			<NavLink activeClassName='my-active' to='/about/vision'>
-				Our vision
-			</NavLink>
-			<Route component={Team} path='/about/team' />
-			<Route component={Vision} path='/about/vision' />
-		</section>
-	);
+	return <section className='about'></section>;
 }
